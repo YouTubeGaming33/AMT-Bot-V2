33b7fd2998f7fefd8ba8b22c38380a6db592df0c
YouTubeGaming33 <60320682+YouTubeGaming33@users.noreply.github.com>
2025-07-16 18:35:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# Required Discord Library(s)
+import discord
+from discord.ext import app_commands
 
+# Required DOTENV Library(s)
+import os
+from dotenv import load_dotenv()
+
+# Required Asyncio Library - Organisation 
+import asyncio
+
+# Load Token and Specify Guild
+load_dotenv()
+TOKEN = os.dotenv("DISCORD_TOKEN")
+GUILD_ID = discord.Object(id=925867675513659473)
+
+# Bot Class - Initiates, Loads Cog(s), Syncs Commands

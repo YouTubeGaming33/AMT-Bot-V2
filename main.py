a59b38d807ae69044d0d2c9c263493b6ccb5844d
YouTubeGaming33 <wobblewibble03@gmail.com>
2025-07-16 23:09:01 +0100

    + Fully Setup main.py to function and load cog(s)

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
-# Required Discord Library(s)
+# Required Discord Library(s).
 import discord
-from discord.ext import app_commands
+from discord.ext import commands
 
-# Required DOTENV Library(s)
+# Required DOTENV Library(s).
 import os
 from dotenv import load_dotenv
 
-# Required Asyncio Library - Organisation 
+# Required Asyncio Library - Organisation. 
 import asyncio
 
-# Load Token and Specify Guild
+# Load Token and Specify Guild.
 load_dotenv()
 TOKEN = os.dotenv("DISCORD_TOKEN")
 GUILD_ID = discord.Object(id=925867675513659473)
 
-# Bot Class - Initiates, Loads Cog(s), Syncs Commands
+# Set Intents to .all - Allows for Use of All Intents without adding additional.
+intents = discord.Intents.all()
+
+# Bot Class - Initiates, Loads Cog(s), Syncs Commands.
+class AMTBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="!", intents=intents)
+
+    async def setup_hook(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                await self.load_extension(f"cogs.{filename[:-3]}")
+                print(f"Successfully Loaded Cog: {filename}")
+            
+        try:
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f"Successfully Synced {len(synced)} Command(s)")
+        except Exception as e:
+            print(f"Failed to Sync Commands: {e}")
+
+# Makes AMTBOT() Class Function into a Variable.
+bot = AMTBot()
+
+# Bot Event for when Ready.
+@bot.event
+async def on_ready():
+    print(f"Logged on as {bot.user}")
+
+# Async Function for Starting Bot.
+async def main():
+    async with bot:
+        await bot.start(TOKEN)
+
+# Asyncio Run Command for Main Function.
+asyncio.run(main())

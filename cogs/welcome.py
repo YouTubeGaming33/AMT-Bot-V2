bf5a6f41b1884fb0d36c0e597be34e7c341844a3
YouTubeGaming33 <wobblewibble03@gmail.com>
2025-07-16 23:14:54 +0100

    + Added Basic Code for welcome.py

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
 
+from main import GUILD_ID
+
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_member_join(self):
+        print ("test")
+
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))

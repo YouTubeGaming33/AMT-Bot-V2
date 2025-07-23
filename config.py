b6e06a0a9d44c54cd466fae809cfaf43295adbeb
YouTubeGaming33 <wobblewibble03@gmail.com>
2025-07-23 15:28:39 +0100

    + Commented config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+# Required Libary(s) and Import(s)
 import os
 from dotenv import load_dotenv
+
+# Loads .env File
 load_dotenv()
 
+# Pulls Mongo URI from .env File
 MONGO_URI = os.getenv("MONGO_URI")
 
+# Guild ID for AMT Server
 GUILD_ID = 925867675513659473
-
-# Custom Emojis
-
